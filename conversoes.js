d5ac4f1c657f06afddf9b96546cd6d33e29b72d0
de Andrade Silva <didv@gft.com>
2022-03-03 17:47:45 -0300

    inclusão conversoes.js

---
+++ conversoes.js
@@ -0,0 +1,12 @@
+// tipo de dado
+// booleanos
+
+// conversão implícita
+const numero = 756;
+const numeroString = Number("756a");
+
+//Number()
+//String()
+console.log(numero + numeroString)
+
+// conversão explícita
