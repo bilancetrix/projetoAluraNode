885a256735c9c940d619f877ccad081e024c0593
de Andrade Silva <didv@gft.com>
2022-03-03 14:56:39 -0300

    first commit

---
+++ tipoBoolean.js
@@ -0,0 +1,13 @@
+// Boolean
+// true -> positivo, verdadeiro
+// false -> negativo, errado
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+
+console.log(primeiroNumero === segundoNumero)
+
+const texto1 = "Alura";
+const texto2 = "Alura";
+
+console.log(texto1 === texto2)
---
+++ tipoString.js
@@ -0,0 +1,13 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura256";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Davi"
+
+//concatenação (+)
+
+
+
+//template string ou template literal
