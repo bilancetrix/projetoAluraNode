2d5b577a99ba537d254c711af20cd971d3e50420
de Andrade Silva <didv@gft.com>
2022-03-04 16:24:05 -0300

    progresso do curso 60%

--- conversoes.js
+++ conversoes.js
@@ -2,11 +2,24 @@
 // booleanos
 
 // conversão implícita
-const numero = 756;
-const numeroString = Number("756a");
+// const numero = 756;
+// const numeroString = Number("756a");
 
 //Number()
 //String()
-console.log(numero + numeroString)
+// console.log(numero + numeroString)
+
+var respostaDeTudo = 42
+let idade = 29
+const pi = 3.14
+
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+    console.log(respostaDeTudo, idade, pi)
+
+}
+console.log(respostaDeTudo, idade, pi)
 
 // conversão explícita
